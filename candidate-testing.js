83147afc3b57d002f7510da748a01c9f429dbf30
Anusha <anusharg85@gmail.com>
2021-02-18 05:31:21 +0000

    Added grade declaration

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,10 +8,11 @@ let candidateName = "";
 let question = "Who was the first american woman in space?";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space?","True or false: 5000 meters == 5 kilometers.","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
+let questions = ["Who was the first American woman in space?","True or false: 5000 meters == 5 kilometers.","(5 + 3)/2 * 10 = ?","Given the array [8,'Orbit','Trajectory',45],what entry is at index 2?","What is the minimum crew size for the ISS?"];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 let score = 0;
+let grade = 0;
 
 
 
@@ -51,12 +52,12 @@ function askQuestion() {
 
     console.log(`5) "What is the minimum crew size for the ISS?"\nyour Answer: ${candidateAnswers[4]}\ncorrect Answer: ${correctAnswers[4]}`);
 
-    let grade = score / questions.length * 100
+    grade = score / questions.length * 100
     if (grade > 80) {
       console.log(`>>> overall grade: ${grade}`);
       console.log(`status: pass`);
     } else if (grade < 80) {
-      console.log(`>>>overall grade: ${grade}`);
+      console.log(`>>>overall grade: ${grade}<<<`);
       console.log(`>>> status: failed <<<`);
     }
   }
