b2f7d2aeaf36207462943453fdc4ff1987a8aa83
Anusha <anusharg85@gmail.com>
2021-02-18 05:52:25 +0000

    console log update

--- candidate-testing.js
+++ candidate-testing.js
@@ -46,10 +46,10 @@ function askQuestion() {
 
     console.log(`5) "What is the minimum crew size for the ISS?"\nyour Answer: ${candidateAnswers[4]}\ncorrect Answer: ${correctAnswers[4]}`);
 
-    grade = score / questions.length * 100
+    grade = score / questions.length * 100;
 
-    if (grade > 80) {
-      console.log(` overall grade: ${grade}1`;
+    if(grade > 80) {
+      console.log(` overall grade: ${grade}`);
       console.log("status: pass");
     } else if (grade < 80) {
       console.log(`overall grade: ${grade}`);
